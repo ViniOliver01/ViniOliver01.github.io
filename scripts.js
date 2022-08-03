5d86503fbe38e794be32fb39bea373d59a3fa132
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-08-03 20:32:37 -0300

    refactored style and navigation

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 function ScrollBt(id) {
-    var str = id; 
-    var id = str.slice(2);
+    id.slice(0, 2) == 'go' ? id=id.slice(2) : null ;
+    
     var items = ['home', 'tools', 'projects', 'contact'];
     var itemIndex = items.indexOf(`${id}`);
     items.splice(itemIndex, 1)
-    var id = document.getElementById(`to${id}`);
+    var id = document.getElementById(`go${id}`);
     id.classList.add("clickedNavItem");
 
     items.map(val => {
-        var val = document.getElementById(`to${val}`);
+        var val = document.getElementById(`go${val}`);
         val.classList.remove("clickedNavItem");
     })
+  }
+
+  function sectionHover(id){
+    ScrollBt(id);
   }
